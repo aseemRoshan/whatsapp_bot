4202e0423b4c2b4beb5025011f18350b96154df0
Aseem Roshan <aseemroshan86@gmail.com>
2025-03-25 13:39:07 +0530

    database to atlas

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/whatsapp_task_tracker');
+    await mongoose.connect('mongodb+srv://Ecommerse:aseem7771@cluster0.bamqt.mongodb.net/Whatsapp_bot?retryWrites=true&w=majority&appName=Cluster0');
     console.log('MongoDB Connected');
   } catch (error) {
     console.error('MongoDB Connection Error:', error);
